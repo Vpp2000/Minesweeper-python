b127dfd26235ff98ab43e06c5a84e7a4279fc96b
Vpp2000 <victor.ponce.p@uni.pe>
2022-05-01 19:03:17 -0500

    NOw the cells show the amount of surrounder mines

--- cell.py
+++ cell.py
@@ -23,17 +23,52 @@ class Cell:
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
-        print(f'Left click... ${event}')
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
 
     def right_click_actions(self, event):
         print(f'Right click... ${event}  ')
 
     @staticmethod
     def randomized_mines():
-        picked_cells = random.sample(Cell.all, settings.MINES_IN_GAME)
+        picked_cells = random.sample(Cell.game_cells, settings.MINES_IN_GAME)
 
         for picked_cell in picked_cells:
             picked_cell.is_mine = True
+
+    def show_mine(self):
+        self.cell_btn_object['text'] = 'MINE'
+        self.cell_btn_object["bg"] = "red"
+
+    def show_cell(self):
+        self.cell_btn_object['text'] = self.surrounded_mines_length
+
+    @property
+    def surrounded_cells(self):
+        surrounded_cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1),
+        ]
+        surrounded_cells = [cell for cell in surrounded_cells if cell is not None]
+
+        return surrounded_cells
+
+    @property
+    def surrounded_mines_length(self):
+        return len([cell for cell in self.surrounded_cells if cell.is_mine])
+
+    def get_cell_by_axis(self, x, y):
+        for cell in Cell.game_cells:
+            if cell.x == x and cell.y == y:
+                return cell
 
     def __repr__(self):
         return f"Cell(y={self.y},x={self.x})"
